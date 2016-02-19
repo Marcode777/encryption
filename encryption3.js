27644302e07d6f691047387422d5a403e6dca187
Marcode777 <marc102202@hotmail.com>
2016-02-19 05:08:20 -0500

    added encryption.js examples

---
+++ encryption3.js
@@ -0,0 +1,7 @@
+// Create a new DB
+// Create an express / handlebars / node.js / mysql app
+// Make a page that will allow users create a user name and password
+// Make a page that will allow users to login
+// Valiate the name so it's only strings, and password that it has at least 8 characters
+// Setup the model and save password into the database as hash
+// Check the password against the hash before login
